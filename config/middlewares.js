bb94606923c979ee22c05caa51e7969907eb2fa8
Yohan <yohanlahiru86@gmail.com>
2025-07-30 17:34:05 +0530

    Fix CORS and CSP for GitHub Pages frontend

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,31 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://arf-strapi-app.onrender.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://arf-strapi-app.onrender.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'https://yohan86.github.io',
+        'https://arf-strapi-app.onrender.com',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
