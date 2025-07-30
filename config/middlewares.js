7d1725d3ec0dc61413f46ff4b4d8f719640005ba
Yohan <yohanlahiru86@gmail.com>
2025-07-30 18:10:12 +0530

    updated cors and security

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,9 +6,12 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
+          'default-src': ["'self'", 'https:', 'http:'],
           'img-src': ["'self'", 'data:', 'blob:', 'https://arf-strapi-app.onrender.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'https://arf-strapi-app.onrender.com'],
           'connect-src': ["'self'", 'https:', 'http:'],
+          'font-src': ["'self'", 'https:', 'data:'],
+          'script-src': ["'self'", 'https:'],
           upgradeInsecureRequests: null,
         },
       },
@@ -24,7 +27,8 @@ module.exports = [
         'https://arf-strapi-app.onrender.com',
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'Range'],
+      expose: ['Content-Range'],
       keepHeaderOnError: true,
     },
   },
